bddc91538784dbf445d545fef915ddba920f78d5
marshmellow2178 <115971843+marshmellow2178@users.noreply.github.com>
2025-10-16 16:25:43 +0900

    Initial commit

---
+++ src/main/java/com/init330/sandboxtest/SandboxTestApplication.java
@@ -0,0 +1,13 @@
+package com.init330.sandboxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/init330/sandboxtest/SandboxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.init330.sandboxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
